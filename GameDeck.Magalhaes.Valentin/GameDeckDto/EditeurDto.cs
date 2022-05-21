80a2c2bc603b1d1b1751f16916039581755202f5
Thomas MAGALHAES <thomas.magalhaes00@gmail.com>
2022-05-21 10:21:08 +0200

    Ajout des DTO

---
+++ GameDeck.Magalhaes.Valentin/GameDeckDto/EditeurDto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace GameDeckDto
+{
+    /// <summary>
+    /// Represente le dto de l'entite <see cref="Editeur"/>.
+    /// </summary>
+    public class EditeurDto
+    {
+        /// <summary>
+        /// Obtient ou definit l'identifiant de l'<see cref="EditeurDto"/>.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Obtient ou definit le nom de l'<see cref="EditeurDto"/>.
+        /// </summary>
+        public string Nom { get; set; }
+
+        /// <summary>
+        /// Obtient ou definit la liste de <see cref="JeuDto"/> de l'<see cref="EditeurDto"/>.
+        /// </summary>
+        public ICollection<JeuDto> Jeux { get; set; } = new List<JeuDto>();
+    }
+}
---
+++ GameDeck.Magalhaes.Valentin/GameDeckDto/GenreDto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace GameDeckDto
+{
+    /// <summary>
+    /// Represente le dto de l'entite <see cref="Genre"/>.
+    /// </summary>
+    public class GenreDto
+    {
+        /// <summary>
+        /// Obtient ou definit l'identifiant du <see cref="GenreDto"/>.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Obtient ou definit le nom du <see cref="GenreDto"/>.
+        /// </summary>
+        public string Nom { get; set; }
+
+        /// <summary>
+        /// Obtient ou definit la liste de <see cref="JeuDto"/> du <see cref="GenreDto"/>.
+        /// </summary>
+        public ICollection<JeuDto> Jeux { get; set; } = new List<JeuDto>();
+    }
+}
