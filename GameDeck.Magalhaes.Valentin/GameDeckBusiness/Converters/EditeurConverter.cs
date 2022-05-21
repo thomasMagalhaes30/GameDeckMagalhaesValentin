174fe569f8c43093ca28b4d6d5ece808471de2bb
Thomas MAGALHAES <thomas.magalhaes00@gmail.com>
2022-05-21 11:05:25 +0200

    ajout des converters

---
+++ GameDeck.Magalhaes.Valentin/GameDeckBusiness/Converters/EditeurConverter.cs
@@ -0,0 +1,89 @@
+﻿using GameDeckDto;
+using Modele.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameDeckBusiness.Converters
+{
+    /// <summary>
+    /// Represente le converter de l'entite <see cref="Editeur"/> en dto  <see cref="EditeurDto"/> et vice-versa.
+    /// </summary>
+    public static class EditeurConverter
+    {
+        #region [ Converter ]
+
+        /// <summary>
+        /// Convertie l'entites <see cref="Editeur"/> en dto <see cref="EditeurDto"/>.
+        /// </summary>
+        /// <param name="entity">L'entite a convertir en dto</param>
+        public static EditeurDto ConvertToDto(Editeur entity)
+        {
+            return new EditeurDto
+            {
+                Id = entity.Id,
+                Nom = entity.Nom,
+                Jeux = JeuConverter.ConvertToDto(entity.Jeux.ToList()),
+            };
+        }
+
+        /// <summary>
+        /// Convertie le dto <see cref="EditeurDto"/> en entite <see cref="Editeur"/>.
+        /// </summary>
+        /// <param name="dto">Le dto a convertir en entite</param>
+        public static Editeur ConvertToEntity(EditeurDto dto)
+        {
+            return new Editeur
+            {
+                Id = dto.Id,
+                Nom = dto.Nom,
+                Jeux = JeuConverter.ConvertToEntity(dto.Jeux.ToList()),
+            };
+        }
+
+        #endregion
+
+        #region [ Converter List ]
+
+        /// <summary>
+        /// Convertie les entites <see cref="Editeur"/> en dtos <see cref="EditeurDto"/>.
+        /// </summary>
+        /// <param name="entities">Les entites a convertir en dto</param>
+        public static List<EditeurDto> ConvertToDto(List<Editeur> entities)
+        {
+            // Si la collection est null ou vide
+            if (entities?.FirstOrDefault() == null)
+            {
+                return null;
+            }
+
+            List<EditeurDto> dtos = new List<EditeurDto>();
+            foreach (Editeur item in entities)
+            {
+                dtos.Add(ConvertToDto(item));
+            }
+            return dtos;
+        }
+
+        /// <summary>
+        /// Convertie les dtos <see cref="EditeurDto"/> en entities <see cref="Editeur"/>.
+        /// </summary>
+        /// <param name="dtos">Les dtos a convertir en entite</param>
+        public static List<Editeur> ConvertToEntity(List<EditeurDto> dtos)
+        {
+            // Si la collection est null ou vide
+            if (dtos?.FirstOrDefault() == null)
+            {
+                return null;
+            }
+
+            List<Editeur> entites = new List<Editeur>();
+            foreach (EditeurDto item in dtos)
+            {
+                entites.Add(ConvertToEntity(item));
+            }
+            return entites;
+        }
+
+        #endregion
+    }
+}
---
+++ GameDeck.Magalhaes.Valentin/GameDeckBusiness/Converters/EvaluationConverter.cs
@@ -0,0 +1,95 @@
+﻿using GameDeckDto;
+using Modele.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameDeckBusiness.Converters
+{
+    /// <summary>
+    /// Represente le converter de l'entite <see cref="Evaluation"/> en dto  <see cref="EvaluationDto"/> et vice-versa.
+    /// </summary>
+    public static class EvaluationConverter
+    {
+        #region [ Converter ]
+
+        /// <summary>
+        /// Convertie l'entites <see cref="Evaluation"/> en dto <see cref="EvaluationDto"/>.
+        /// </summary>
+        /// <param name="entity">L'entite a convertir en dto</param>
+        public static EvaluationDto ConvertToDto(Evaluation entity)
+        {
+            return new EvaluationDto
+            {
+                Id = entity.Id,
+                NomEvaluateur = entity.NomEvaluateur,
+                Date = entity.Date,
+                Note = entity.Note,
+                JeuId = entity.JeuId,
+                //JeuObj = JeuConverter.ConvertToDto(entity.JeuObj),
+            };
+        }
+
+        /// <summary>
+        /// Convertie le dto <see cref="EvaluationDto"/> en entite <see cref="Evaluation"/>.
+        /// </summary>
+        /// <param name="dto">Le dto a convertir en entite</param>
+        public static Evaluation ConvertToEntity(EvaluationDto dto)
+        {
+            return new Evaluation
+            {
+                Id = dto.Id,
+                NomEvaluateur = dto.NomEvaluateur,
+                Date = dto.Date,
+                Note = dto.Note,
+                JeuId = dto.JeuId,
+                //JeuObj = JeuConverter.ConvertToDto(dto.JeuObj),
+            };
+        }
+
+        #endregion
+
+        #region [ Converter List ]
+
+        /// <summary>
+        /// Convertie les entites <see cref="Evaluation"/> en dtos <see cref="EvaluationDto"/>.
+        /// </summary>
+        /// <param name="entities">Les entites a convertir en dto</param>
+        public static List<EvaluationDto> ConvertToDto(List<Evaluation> entities)
+        {
+            // Si la collection est null ou vide
+            if (entities?.FirstOrDefault() == null)
+            {
+                return null;
+            }
+
+            List<EvaluationDto> dtos = new List<EvaluationDto>();
+            foreach (Evaluation item in entities)
+            {
+                dtos.Add(ConvertToDto(item));
+            }
+            return dtos;
+        }
+
+        /// <summary>
+        /// Convertie les dtos <see cref="EvaluationDto"/> en entities <see cref="Evaluation"/>.
+        /// </summary>
+        /// <param name="dtos">Les dtos a convertir en entite</param>
+        public static List<Evaluation> ConvertToEntity(List<EvaluationDto> dtos)
+        {
+            // Si la collection est null ou vide
+            if (dtos?.FirstOrDefault() == null)
+            {
+                return null;
+            }
+
+            List<Evaluation> entites = new List<Evaluation>();
+            foreach (EvaluationDto item in dtos)
+            {
+                entites.Add(ConvertToEntity(item));
+            }
+            return entites;
+        }
+
+        #endregion
+    }
+}
