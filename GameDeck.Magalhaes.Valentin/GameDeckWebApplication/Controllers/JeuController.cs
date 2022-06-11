ca9f55ccf0766e5b2531a1dd2e43fe528bf2f36b
Thomas MAGALHAES <thomas.magalhaes00@gmail.com>
2022-06-11 09:10:04 +0200

    ajout des certainnes vue

---
+++ GameDeck.Magalhaes.Valentin/GameDeckWebApplication/Controllers/JeuController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameDeckWebApplication.Controllers
+{
+    public class JeuController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
