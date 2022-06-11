6c0f07eaf7d1cd094bfd6d854960c8414385ba4d
Thomas MAGALHAES <thomas.magalhaes00@gmail.com>
2022-06-11 11:51:52 +0200

    bootstrap 5 :)

--- GameDeck.Magalhaes.Valentin/GameDeckWebApplication/Controllers/EditeurController.cs
+++ GameDeck.Magalhaes.Valentin/GameDeckWebApplication/Controllers/EditeurController.cs
@@ -1,5 +1,6 @@
 ﻿using GameDeckBusiness;
 using GameDeckDto;
+using GameDeckWebApplication.Models;
 using GameDeckWebApplication.Models.Converters;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,28 @@ namespace GameDeckWebApplication.Controllers
         {
             List<EditeurDto> dtos = Manager.GetInstance().GetAllEditeurs();
             return View(EditeurAdapter.ConvertToVM(dtos));
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            EditeurDto dto = new EditeurDto();
+            if (id.HasValue)
+            {
+                dto = Manager.GetInstance().GetOneEditeur(id.Value);
+            }
+
+            return View(EditeurAdapter.ConvertToVM(dto));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditeurVM vm)
+        {
+            if (vm == null || !ModelState.IsValid)
+            {
+                return Json(new { success = false, errors = ModelState.Keys.SelectMany(key => ModelState[key].Errors).Select(msg => msg.ErrorMessage) });
+            }
+
+            return View(vm);
         }
     }
 }
