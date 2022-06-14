bd95b9f1c6cafe8523865074834b27f6b7ae21f8
thomasMagalhaes30 <thomas.magalhaes00@gmail.com>
2022-06-14 15:33:51 +0200

    refonte graphique page principale + editeur

---
+++ GameDeck.Magalhaes.Valentin/GameDeckWebApplication/Models/Extentions/HtmlUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameDeckWebApplication.Models.Extentions
+{
+    /// <summary>
+    /// Represente une classe d'extention de l'<see cref="HtmlHelper"/>.
+    /// </summary>
+    public static class HtmlUtils
+    {
+        /// <summary>
+        /// Indique si un lien est actif ou non.
+        /// </summary>
+        /// <param name="actionName">le nom de l'action</param>
+        /// <param name="controllerName">le nom du controlleur</param>
+        /// <returns>"active" si il est actif, "" sinon </returns>
+        public static string IsActive(this HtmlHelper html, string actionName, string controllerName)
+        {
+            var routeData = html.ViewContext.RouteData;
+
+            var routeAction = (string)routeData.Values["action"];
+            var routeControl = (string)routeData.Values["controller"];
+
+            // must match both
+            var returnActive = controllerName == routeControl &&
+                               actionName == routeAction;
+
+            return returnActive ? "active" : "";
+        }
+    }
+}
