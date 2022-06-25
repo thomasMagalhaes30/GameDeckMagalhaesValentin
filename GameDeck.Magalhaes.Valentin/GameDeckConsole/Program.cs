daf13eb95b37539748a46b37df87d8163086256b
thomasMagalhaes30 <thomas.magalhaes00@gmail.com>
2022-06-25 14:27:55 +0200

    ajout jeux console + rework jeux

--- GameDeck.Magalhaes.Valentin/GameDeckConsole/Program.cs
+++ GameDeck.Magalhaes.Valentin/GameDeckConsole/Program.cs
@@ -27,8 +27,10 @@ namespace GameDeckConsole
                 // GENRE
                 GenreDto g1 = new GenreDto { Nom = "Course" };
                 GenreDto g2 = new GenreDto { Nom = "Aventure" };
+                GenreDto g3 = new GenreDto { Nom = "Famille" };
                 g1.Id = Manager.GetInstance().AddGenre(g1);
                 g2.Id = Manager.GetInstance().AddGenre(g2);
+                g3.Id = Manager.GetInstance().AddGenre(g3);
                 // lecture des genres
                 Console.WriteLine("\nListe de mes genres : ");
                 Manager.GetInstance().GetAllGenres().ForEach(g => Console.WriteLine("Genre n°{0} : {1}", g.Id, g.Nom));
@@ -60,15 +62,26 @@ namespace GameDeckConsole
                     GenreId = g2.Id,
                     EditeurId = ed2.Id
                 };
+                JeuDto j3 = new JeuDto
+                {
+                    Nom = "Super Mario Party Switch",
+                    Description = "La série Mario Party débarque sur Nintendo Switch avec un gameplay et des mini-jeux survoltés pour tous ! Le jeu de plateau original a été agrémenté de nouveaux éléments stratégiques, comme les dés propres à chaque personnage.",
+                    DateSortie = new DateTime(2018, 10, 5, 0, 0, 0),
+                    GenreId = g3.Id,
+                    EditeurId = ed2.Id
+                };
                 j1.Id = Manager.GetInstance().AddJeu(j1);
                 j2.Id = Manager.GetInstance().AddJeu(j2);
+                j3.Id = Manager.GetInstance().AddJeu(j3);
 
                 // EVALUATION
                 Console.WriteLine("Ajout des évaluations");
                 EvaluationDto eval1 = new EvaluationDto { NomEvaluateur = "Guillaume", Date = new DateTime(2022, 6, 4, 9, 54, 10), Note = 14.7f, JeuId = j1.Id };
                 EvaluationDto eval2 = new EvaluationDto { NomEvaluateur = "Thomas", Date = new DateTime(2022, 6, 4, 9, 55, 30), Note = 18.5f, JeuId = j1.Id };
+                EvaluationDto eval3 = new EvaluationDto { NomEvaluateur = "James", Date = new DateTime(2022, 6, 25, 9, 55, 30), Note = 15f, JeuId = j3.Id };
                 eval1.Id = Manager.GetInstance().AddEvaluation(eval1);
                 eval2.Id = Manager.GetInstance().AddEvaluation(eval2);
+                eval3.Id = Manager.GetInstance().AddEvaluation(eval3);
 
                 // EXPERIENCE
                 Console.WriteLine("Ajout des expériences");
